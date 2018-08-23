82fbda162b3d348c4730cd287f44a4afbdb77f0b
Айдос Мелдеханов <C:\Users\web-prog\AppData\Roaming\The Bat!>
2018-08-23 18:43:56 +0600

    add comments for news

--- news/admin.py
+++ news/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from news.models import News, Tag, Category
+from news.models import News, Tag, Category, Comments
 
 
 class NewsAdmin(SummernoteModelAdmin):
-    summernote_fields = ('text_min','text')
+    summernote_fields = ('text_min', 'text')
 
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Tag)
 admin.site.register(Category)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'new', 'text', 'created', 'moderation')
+
+
+admin.site.register(Comments, CommentAdmin)
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from news.models import News
+from django.shortcuts import render, get_object_or_404, redirect
+from news.models import News, Comments
+from news.forms import CommentForm
 
 
 def news_list(request):
@@ -13,4 +14,18 @@ def new_single(request, pk):
     """Вывод полной статьи
     """
     new = get_object_or_404(News, id=pk)
-    return render(request, "news/new_single.html", {"new": new})
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.user = request.user
+            form.new = new
+            form.save()
+            return redirect(new_single, pk)
+    else:
+        comments = Comments.objects.filter(new=pk, moderation=True)
+        form = CommentForm()
+        return render(request, "news/new_single.html",
+                      {'new': new,
+                       'comments': comments,
+                       'form': form})
